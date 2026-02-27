bacaf768821b311df6755d47dd3db2b27037d5ee
Challagandla Anantha Pavan <ananthapavanchallagandla@gmail.com>
2026-02-27 17:18:04 +0530

    First commit

---
+++ university.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+st.title("Simple Course Registration")
+
+courses = {
+    "CSE101": {"name": "Python", "fee": 5000, "seats": 2},
+    "ECE201": {"name": "Digital Circuits", "fee": 4000, "seats": 1}
+}
+
+if "registrations" not in st.session_state:
+    st.session_state.registrations = []
+
+if "seat_count" not in st.session_state:
+    st.session_state.seat_count = {code: 0 for code in courses}
+
+menu = st.sidebar.selectbox("Menu", ["Register", "View All", "Revenue"])
+
+if menu == "Register":
+
+    st.header("Register Student")
+
+    name = st.text_input("Student Name")
+    roll = st.number_input("Roll Number", min_value=1, step=1)
+    course_code = st.selectbox("Select Course", list(courses.keys()))
+
+    if st.button("Register"):
+        if name == "":
+            st.error("Enter Name")
+        else:
+            if st.session_state.seat_count[course_code] < courses[course_code]["seats"]:
+
+                st.session_state.registrations.append({
+                    "name": name,
+                    "roll": roll,
+                    "course": courses[course_code]["name"],
+                    "fee": courses[course_code]["fee"]
+                })
+
+                st.session_state.seat_count[course_code] += 1
+
+                st.success("Registered Successfully!")
+            else:
+                st.warning("No seats available!")
+
+elif menu == "View All":
+
+    st.header("All Registrations")
+
+    if st.session_state.registrations:
+        st.table(st.session_state.registrations)
+    else:
+        st.info("No students registered yet.")
+
+elif menu == "Revenue":
+
+    st.header("Total Revenue")
+
+    total = sum(r["fee"] for r in st.session_state.registrations)
+
+    st.success(f"Total Revenue: ₹{total}")
